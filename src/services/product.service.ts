60468daf9b96da3569bf46eb000c495e9690e413
NicolasPreiti <nicolaspreiti45@gmail.com>
2022-12-20 17:10:45 -0300

    product service updates

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,6 +1,8 @@
+import { PutObjectCommandInput } from '@aws-sdk/client-s3'
 import { CreateProductDTO } from '../dto/product/create-product.dto'
 import { UpdateProductDTO } from '../dto/product/update-product.dto'
 import { Uuid } from '../helpers/uuid.helper'
+import { AwsS3Client } from '../libs/aws-s3'
 import { ProductModel } from '../models/product.model'
 import { ProductRepository } from '../repositories/product.repository'
 
@@ -8,18 +10,34 @@ export class ProductService {
   private readonly uuid: Uuid
 
   constructor (
-    private readonly repository: ProductRepository
+    private readonly repository: ProductRepository,
+    private readonly s3Client: AwsS3Client
   ) {
     this.uuid = new Uuid()
   }
 
-  public create = async (body: CreateProductDTO): Promise<ProductModel> => {
+  public create = async (body: CreateProductDTO, image: Express.Multer.File): Promise<ProductModel> => {
     const uuid = this.uuid.generate()
+    const filename = image.originalname
 
     const product = await this.repository.create({
       uuid,
+      image: filename,
       ...body
     })
+
+    // AWS
+    const params: PutObjectCommandInput = {
+      Bucket: 'refrigeracion',
+      Key: filename,
+      Body: image.buffer
+    }
+    await this.s3Client.putObject(params)
+    const imageUrl = await this.s3Client.getSignedUrl({
+      Bucket: params.Bucket,
+      Key: params.Key
+    })
+    product.image = imageUrl
 
     return product
   }
@@ -30,11 +48,29 @@ export class ProductService {
       offset
     })
 
+    // Call image from aws s3
+    await Promise.all(products.map(async (product) => {
+      const imageKey = product.image
+      const imageUrl = await this.s3Client.getSignedUrl({
+        Bucket: 'refrigeracion',
+        Key: imageKey
+      })
+
+      product.image = imageUrl
+    }))
+
     return products
   }
 
   public findOne = async (uuid: string): Promise<ProductModel> => {
     const product = await this.repository.findOne(uuid)
+
+    const imageUrl = await this.s3Client.getSignedUrl({
+      Bucket: 'refrigeracion',
+      Key: product.image
+    })
+
+    product.image = imageUrl
     return product
   }
 
